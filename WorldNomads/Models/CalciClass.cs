d1016369b8331ff1637b869c6cc3a27d67150170
arunavelile <varuna0106@gmail.com>
2018-05-05 03:33:29 +1000

    WorldNomads

---
+++ WorldNomads/Models/CalciClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorldNomads.Models
+{
+    public class CalciClass
+    {
+        public int num { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Enter only a Valid Number")]
+        public int inpNum { get; set; }
+        public bool successCritera { get; set; }
+    }
+}
