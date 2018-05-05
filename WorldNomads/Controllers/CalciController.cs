d6386d7f5c2e8ca5130f94fb9caf9ebbc7489718
arunavelil <varuna0106@gmail.com>
2018-05-05 23:07:32 +1000

    Revert "Revert "WorldNomads""
    This reverts commit f3ff2d913e1a0bc1c80e877ef39779904d8eb2c2.

---
+++ WorldNomads/Controllers/CalciController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorldNomads.Models;
+
+namespace WorldNomads.Controllers
+{
+    public class CalciController : Controller
+    {
+        // GET: Calc
+        public ActionResult Calc()
+        {
+            CalciClass Cal = new CalciClass();
+            return View(Cal);
+        }
+        [HttpPost]
+        public ActionResult Calc(CalciClass Model)
+        {
+            if (ModelState.IsValid)
+            {
+                Model.successCritera = true;
+            }
+            return View(Model);
+        }
+    }
+}
